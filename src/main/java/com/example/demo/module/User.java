60f7b438d5155b2ce5cbcee4a41c292689ca2358
User <huseynovali612@gmail.com>
2024-04-16 18:55:06 +0400

    create user

--- src/main/java/com/example/demo/module/User.java
+++ src/main/java/com/example/demo/module/User.java
@@ -25,7 +25,7 @@ public class User implements UserDetails {
     private String name;
     private String email;
     private String password;
-
+    private String ProfilePicture;
     @Enumerated(EnumType.STRING)
     private Role role;
 
